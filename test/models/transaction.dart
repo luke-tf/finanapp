99e16ad3f2c39c884c907db895081248d4d07220
Lucas Toledo Fernandes <lukero.toledo@gmail.com>
2025-09-04 15:12:34 -0300

    Adding some tests

---
+++ test/models/transaction.dart
@@ -0,0 +1,30 @@
+import 'package:hive/hive.dart';
+
+part 'transaction.g.dart';
+
+@HiveType(typeId: 0)
+class Transaction extends HiveObject {
+  @HiveField(0)
+  late String title;
+
+  @HiveField(1)
+  late double value;
+
+  @HiveField(2)
+  late DateTime date;
+
+  @HiveField(3)
+  late bool isExpense;
+
+  Transaction({
+    this.title = '',
+    this.value = 0.0,
+    DateTime? date,
+    this.isExpense = true,
+  }) : date = date ?? DateTime.now(); // This line handles the default date
+
+  @override
+  String toString() {
+    return 'Transaction(title: $title, value: $value, date: $date, isExpense: $isExpense)';
+  }
+}
